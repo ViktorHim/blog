ba8b976f04ea6fcedfe2c5fb789cf15f586840cb
ViktorHim <vhimlihmail@gmail.com>
2025-04-24 18:00:38 +0300

    add bundle-analyzer

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,17 +2,12 @@ import { RouteName, RoutePaths } from 'shared/config/routes/routeConfig';
 import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './Navbar.module.scss';
 import { AppLink, AppLinkTheme } from 'shared/uikit/AppLink/AppLink';
-import { useEffect } from 'react';
 
 interface NavbarProps {
     className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-    useEffect(() => {
-        throw Error();
-    });
-
     return (
         <div className={classNames(styles.Navbar, [className])}>
             <div className={styles.links}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { resolve } from "path";
+import { resolve } from 'path';
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
-const root = resolve(__dirname, "src");
+const root = resolve(__dirname, 'src');
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      app: resolve(root, "app"),
-      pages: resolve(root, "pages"),
-      widgets: resolve(root, "widgets"),
-      features: resolve(root, "features"),
-      entities: resolve(root, "entities"),
-      shared: resolve(root, "shared"),
-      uikit: resolve(root, "uikit"),
+    plugins: [react(), svgr()],
+    resolve: {
+        alias: {
+            app: resolve(root, 'app'),
+            pages: resolve(root, 'pages'),
+            widgets: resolve(root, 'widgets'),
+            features: resolve(root, 'features'),
+            entities: resolve(root, 'entities'),
+            shared: resolve(root, 'shared'),
+            uikit: resolve(root, 'uikit'),
+        },
     },
-  },
 });
